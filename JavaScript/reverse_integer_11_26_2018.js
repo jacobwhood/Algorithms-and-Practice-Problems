24d3adada7ae8a44d6e8c6269d4c43d4d40229b9
jacobwhood <jacobwhood@utexas.edu>
2018-11-26 09:32:10 -0600

    Add problem description

--- JavaScript/reverse_integer_11_26_2018.js
+++ JavaScript/reverse_integer_11_26_2018.js
@@ -1,4 +1,26 @@
 /**
+ * Reverse Integer
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * 
+ * Example 1:
+ * Input: 123
+ * Output: 321
+ * 
+ * Example 2:
+ * Input: -123
+ * Output: -321
+ * 
+ * Example 3:
+ * Input: 120
+ * Output: 21
+ * 
+ * NOTE:
+ * Assume we are dealing with an environment which could only store integers 
+ * within the 32 - bit signed integer range: [−231, 231− 1].For the purpose of 
+ * this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
+
+/**
  * @param {number} x
  * @return {number}
  */
@@ -22,9 +44,9 @@ var reverse = function (x) {
   }
 };
 
-console.log(reverse(123));
-console.log(reverse(-123));
-console.log(reverse(120));
-console.log(reverse(-2147483648));
-console.log(reverse(1534236469));
-console.log(reverse(-1563847412));
+console.log(reverse(123));            // 321
+console.log(reverse(-123));           // -321
+console.log(reverse(120));            // 21
+console.log(reverse(-2147483648));    // 0
+console.log(reverse(1534236469));     // 0
+console.log(reverse(-1563847412));    // 0
