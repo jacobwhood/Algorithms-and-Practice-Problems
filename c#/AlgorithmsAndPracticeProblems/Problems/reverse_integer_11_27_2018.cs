549d530cd2c78a202dd31273d0c17c03286e8174
jacobwhood <jacobwhood@utexas.edu>
2018-11-27 19:58:19 -0600

    Set up initial unit testing for 3 sum

---
+++ c#/AlgorithmsAndPracticeProblems/Problems/reverse_integer_11_27_2018.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlgorithmsAndPracticeProblems
+{
+    public static class ReverseIntegerClass
+    {
+        public static int ReverseClass(int x)
+        {
+            int result = 0;
+            bool isNegative = x < 0 ? true : false;
+            int copy = x;
+            
+            while (copy != 0)
+            {
+                int digit = copy % 10;
+                result = result * 10 + digit;
+                copy = (int)Math.Floor((double)(copy / 10));
+            }
+
+            if (isNegative)
+                return result <= (Math.Pow(2, 31) * -1) ? 0 : result;
+            else
+                return result >= Math.Pow(2, 31) - 1 ? 0 : result;
+        }
+    }
+}
---
+++ c#/AlgorithmsAndPracticeProblems/Tests/3_sum_11_27_2018Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace AlgorithmsAndPracticeProblems
+{
+    [TestFixture]
+    public class Three_sum_11_27_2018Tests
+    {
+        [Test]
+        public void TestCase1()
+        {
+            int[] example1 = { -1, 0, 1, 2, -1, -4 };
+            
+            ArrayList output = ThreeSumClass.ThreeSum(example1);
+            ArrayList expected = new ArrayList
+            {
+                new int[] { -1, 0, 1 },
+                new int[] { -1, -1, 2 }
+            };
+
+            CollectionAssert.AreEquivalent(output, expected);
+        }
+        
+        [Test]
+        public void TestCase2()
+        {
+            int[] example2 = { -3, 0, 0, 2, 1, -4 };
+            
+            ArrayList output = ThreeSumClass.ThreeSum(example2);
+            ArrayList expected = new ArrayList
+            {
+                new int[] { -3, 1, 2 },
+            };
+
+            CollectionAssert.AreEquivalent(output, expected);
+        }
+    }
+}
