6a76e9d4c3a71d9386772700cd282bd7684b4e50
jacobwhood <jacobwhood@utexas.edu>
2018-11-30 10:36:46 -0600

    Solve Roman Numeral to Int

---
+++ c#/AlgorithmsAndPracticeProblems/Problems/roman_to_integer_11_30_2018.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace AlgorithmsAndPracticeProblems
+{
+    public static class RomanToIntegerClass
+    {
+        
+        public static int RomanToInt(string s)
+        {
+            Hashtable NUMERAL_VALUES = new Hashtable() {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+            
+            int result = 0;
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (!NUMERAL_VALUES.ContainsKey(c))
+                {
+                    return -1;
+                }
+                else
+                {
+                    if ( i + 1 < s.Length && (int)NUMERAL_VALUES[c] < (int)NUMERAL_VALUES[s[i+1]] )
+                    {
+                        result -=(int)NUMERAL_VALUES[c];
+                    }
+                    else
+                    {
+                        result += (int)NUMERAL_VALUES[c];
+                    }
+                }
+                
+            }
+
+
+            return result;
+        }
+    }
+}
