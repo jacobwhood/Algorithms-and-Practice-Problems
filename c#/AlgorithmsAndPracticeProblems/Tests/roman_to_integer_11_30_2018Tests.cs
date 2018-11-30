d8046fc0774ee410f028819f59cfaf508b257109
jacobwhood <jacobwhood@utexas.edu>
2018-11-30 10:37:03 -0600

    Create unit tests for Roman Numeral to Int

---
+++ c#/AlgorithmsAndPracticeProblems/Tests/roman_to_integer_11_30_2018Tests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace AlgorithmsAndPracticeProblems
+{
+    [TestFixture]
+    public class Roman_To_Integer_11_30_2018Tests
+    {
+        [Test]
+        public void TestCase1()
+        {
+            int output = RomanToIntegerClass.RomanToInt("III");
+            Assert.AreEqual(output, 3);
+        }
+        
+        [Test]
+        public void TestCase2()
+        {
+            int output = RomanToIntegerClass.RomanToInt("IV");
+            Assert.AreEqual(output, 4);
+        }
+        
+        [Test]
+        public void TestCase3()
+        {   
+            int output = RomanToIntegerClass.RomanToInt("IX");
+            Assert.AreEqual(output, 9);
+        }
+        
+        [Test]
+        public void TestCase4()
+        {   
+            int output = RomanToIntegerClass.RomanToInt("LVIII");
+            Assert.AreEqual(output, 58);
+        }
+        
+        [Test]
+        public void TestCase5()
+        {   
+            int output = RomanToIntegerClass.RomanToInt("MCMXCIV");
+            Assert.AreEqual(output, 1994);
+        }
+    }
+}
