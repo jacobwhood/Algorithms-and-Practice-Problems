5c4adcc2a05f9be343f2d30b31a8e71d8958360f
jacobwhood <jacobwhood@utexas.edu>
2018-12-04 09:59:53 -0600

    Solve Valid Parentheses and add unit tests

---
+++ c#/AlgorithmsAndPracticeProblems/Problems/valid_parentheses_12_4_2018.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+
+namespace AlgorithmsAndPracticeProblems
+{
+    public static class ValidParenthesesClass
+    {
+        /*
+         * Valid Parentheses
+         * 
+         * Given a string containing just the characters '(', ')', '{', '}', '[', ']', determine
+         * if the input string is valid
+         * 
+         * An input string is valid if:
+         *   1. Open brackets must be closed by the same type of brackets.
+         *   2. Open brackets must be clsoed in the correct order.
+         * 
+         * Example 1:
+         * Input: '()'
+         * Output: true
+         * 
+         * Example 2:
+         * Input: '()[]{}'
+         * Output: true
+         * 
+         * Example 3:
+         * Input: '(]'
+         * Output: false
+         * 
+         * Example 4:
+         * Input: '([)]'
+         * Output false
+         * 
+        */
+        public static bool isValid(string s)
+        {
+            Stack stack = new Stack();
+            
+            string brace = "";
+            
+            Hashtable matchingOpenBrace = new Hashtable()
+            { 
+                {")","("},
+                {"}","{"},
+                {"]", "["}
+            };
+            
+            Hashtable openingBraces = new Hashtable()
+            {
+                {"(", "("},
+                {"{", "{"},
+                {"[", "["}
+            };
+            
+            Hashtable closingBraces = new Hashtable()
+            {
+                {")", ")"},
+                {"}", "}"},
+                {"]", "]"}
+            };
+            
+            foreach (char c in s)
+            {
+                if (openingBraces.ContainsKey(c.ToString()))
+                {
+                    stack.Push(c.ToString());
+                }
+                else if (closingBraces.ContainsKey(c.ToString()))
+                {
+                    if (stack.Count == 0) return false;
+                    
+                    brace = stack.Pop().ToString();
+                    
+                    if (brace != matchingOpenBrace[c.ToString()].ToString())
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            try
+            {
+                if (stack.Pop() != null)
+                {
+                    return false;
+                }
+            }
+            catch(InvalidOperationException e)
+            {
+                // expected behavior
+            }
+            
+            
+            
+            return true;
+        }
+    }
+}
---
+++ c#/AlgorithmsAndPracticeProblems/Tests/valid_parentheses_12_4_2018Tests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace AlgorithmsAndPracticeProblems
+{
+    [TestFixture]
+    public class Valid_Parentheses_12_4_2018Tests
+    {
+        [Test]
+        public void TestCase1()
+        {
+            bool output = ValidParenthesesClass.isValid("()");
+            Assert.IsTrue(output);
+        }
+        
+        [Test]
+        public void TestCase2()
+        {
+            bool output = ValidParenthesesClass.isValid("()[]{}");
+            Assert.IsTrue(output);
+        }
+        
+        [Test]
+        public void TestCase3()
+        {
+            bool output = ValidParenthesesClass.isValid("(]");
+            Assert.IsFalse(output);
+        }
+        
+        [Test]
+        public void TestCase4()
+        {
+            bool output = ValidParenthesesClass.isValid("([)]");
+            Assert.IsFalse(output);
+        }
+    }
+}
